0619fcd8e76fb6119da40354deff731cde54a0d9
Lancelot HARDEL <debetux@gmail.com>
2013-05-07 22:21:04 +0200

    Modification formulaire théâtre

--- application/views/auth/login_include.php
+++ application/views/auth/login_include.php
@@ -35,7 +35,7 @@
 				<div class="row">
 					
 					<div class="small-3 columns hide-for-small">
-						<label for="right-label" class="right inline<?php if(form_error('password')): ?>error<?php endif; ?>">Mot de passe</label>
+						<label for="right-label" class="right inline <?php if(form_error('password')): ?>error<?php endif; ?>">Mot de passe</label>
 					</div>
 					<div class="small-9 columns">
 						<input type="password" id="right-label" name="password" <?php if(form_error('password')): ?>class="error"<?php endif; ?> placeholder="Votre mot de passe...">
--- application/views/dashboard/theaters/add_form_include.php
+++ application/views/dashboard/theaters/add_form_include.php
@@ -10,17 +10,14 @@
 			<?php endif; ?>
 
 
-			<form action="<?php echo site_url('auth/sign_up'); ?>" method="post">
+			<form action="<?php echo site_url('auth/sign_up'); ?>" class="custom" method="post">
 				<div class="row">
-					<div class="large-12 columns">
+					<div class="large-6 columns">
 						<label <?php if(form_error('name')): ?>class="error"<?php endif; ?>>Nom</label>
 
 						<input type="text" name="name" <?php if(form_error('name')): ?>class="error"<?php endif; ?> value="<?php echo set_value('name'); ?>" placeholder="Le nom...">
 						<?php echo form_error('name'); ?>
 					</div>
-				</div>
-				
-				<div class="row">
 					<div class="large-4 columns">
 						<label <?php if(form_error('city')): ?>class="error"<?php endif; ?>>Ville.</label>
 						<input type="text" name="city" <?php if(form_error('city')): ?>class="error"<?php endif; ?> placeholder="La ville...">
@@ -30,11 +27,6 @@
 						<label <?php if(form_error('postal_code')): ?>class="error"<?php endif; ?>>Code Postal.</label>
 						<input type="text" <?php if(form_error('postal_code')): ?>class="error"<?php endif; ?> name="postal_code" placeholder="Code postal...">
 						<?php echo form_error('postal_code'); ?>
-					</div>
-					<div class="large-6 columns">
-						<label <?php if(form_error('phone')): ?>class="error"<?php endif; ?>>Téléphone.</label>
-						<input type="text" <?php if(form_error('phone')): ?>class="error"<?php endif; ?> name="phone" placeholder="Téléphone...">
-						<?php echo form_error('phone'); ?>
 					</div>
 				</div>
 
@@ -47,6 +39,28 @@
 
 				</div>
 				
+			<div id="phone">
+				<div class="row">
+					<div class="large-6 columns">
+						<label <?php if(form_error('phone')): ?>class="error"<?php endif; ?>>Label.</label>
+						<select id="customDropdown">
+							<option>Réservation</option>
+							<option>Contact presse</option>
+							<option>Gérant</option>
+						</select>
+					</div>
+					<div class="large-6 columns">
+						<label <?php if(form_error('phone')): ?>class="error"<?php endif; ?>>Numéro de téléphone.</label>
+						<input type="text" <?php if(form_error('phone')): ?>class="error"<?php endif; ?> name="phone" placeholder="Téléphone...">
+						<?php echo form_error('phone'); ?>
+					</div>
+				</div>
+			</div>
+
+				<div class="row">
+					<div class="large-12 columns"><small><a id="add_phone" href="#">+ Ajouter un numéro de téléphone</a></small></div>
+				</div>
+				
 				<br>
 				<div class="row">
 					<div class="large-12 columns">
@@ -57,3 +71,25 @@
 			</form>
     </div>
   </div>
+	
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
+
+  <script type="text/javascript">
+
+		$("#add_phone").live('click', function(){
+			$("#phone").append('<div class="row" class="custom dropdown">'+
+					'<div class="large-6 columns">'+
+						'<select class="" id="customDropdown">'+
+							'<option>Réservation</option>'+
+							'<option>Contact presse</option>'+
+							'<option>Gérant</option>'+
+						'</select>'+
+					'</div>'+
+					'<div class="large-6 columns">'+
+						'<input type="text" name="phone" placeholder="Téléphone...">'+
+					'</div>'+
+				'</div>');
+		});
+	// Du code utilisant le $() d'autres bibliothèques JS.
+  </script>
