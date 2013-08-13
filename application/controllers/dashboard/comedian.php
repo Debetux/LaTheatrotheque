e3cfddaad0744f42473b715f97628818aee0aea2
Lancelot HARDEL <latheatrotheque@hardel.me>
2013-08-13 18:33:50 +0200

    Ajout de nombreux models

---
+++ application/controllers/dashboard/comedian.php
@@ -0,0 +1,102 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Theaters extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		if(! $this->session->userdata('username')) 
+			redirect('auth/login');
+		$this->output->enable_profiler(TRUE);
+		$this->load->helper('assets');
+		$this->load->model('theaters_model', 'theatersManager');
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+
+		$data['username'] = $this->session->userdata('username');
+		$data['user_theaters'] = $this->theatersManager->list_theaters();
+
+		$this->load->view('templates/header');
+		$this->load->view('dashboard/theaters/theaters', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function add(){
+
+		// Données
+		$this->form_validation->set_error_delimiters('<small class="error">', '</small>');
+		$this->form_validation->set_rules('name', 'nom du théâtre', 'trim|required|min_length[2]|max_length[100]|encode_php_tags|xss_clean|is_unique[theaters.name]');
+		$this->form_validation->set_rules('city', 'ville', 'trim|required|xss_clean|max_length[100]');
+		$this->form_validation->set_rules('postal_code', 'code postal', 'trim|required|xss_clean|exact_length[5]');
+		$this->form_validation->set_rules('address', 'adresse', 'required|xss_clean|encode_php_tags|max_length[140]');
+
+		$data['username'] = $this->session->userdata('username');
+		$data['form']['labels'] = $this->theatersManager->find_labels();
+		$data['form']['labels_js'] = null; # Pour insérer dans le code javascript...
+		$data['form']['labels_html'] = null; # ...et aussi dans le code html. Mais sera sûrement inutile pour le moment.
+
+		# On s'occupe de générer les listes de labels.
+		foreach ($data['form']['labels'] as $key => $label) {
+			$data['form']['labels_js'] .= "'<option value=\"".$label->id."\">".$label->name."</option>'+";
+		}
+
+		# On s'occupe des numéros de téléphone et des mails par des expressions régulières avec une boucle foreach :
+		if(!empty($_POST)){
+			$matches = null; # Pour stocker les numéros des téléphones/mails.
+			foreach ($_POST as $key => $value) {
+				# Mails
+				# On vérifie d'abbord la $key pour déterminer si c'était censé être un numéro de téléphone ou un mail
+				if(preg_match('#mail_address_([0-9]*)#', $key, $matches)){
+					if(preg_match("#^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$#ix", $value)){
+						$mails[$matches[1]] = $value;
+					} else{
+						$mail_errors[$matches[1]] = $value;
+					}
+					$all_mails[$matches[1]] = $value;
+				}
+				# On stocke le label pour bien tout matcher
+				if(preg_match('#mail_label_([0-9]*)#', $key, $matches)) $all_mails_labels[$matches[1]] = $value;
+
+				# Comme on validera automatiquement les numéros de téléphone, il faut juste savoir si il y a des chiffres 
+				if(preg_match('#phone_number_([0-9]*)#', $key, $matches)){
+					if(preg_match('#^(01|02|03|04|05|06|07|08|09)[0-9]{8}$#', $value)){
+						$phone_numbers[$matches[1]] = $value;
+					} else{
+						$phone_errors[$matches[1]] = $key;
+					}
+					$all_phones[$matches[1]] = $value;
+				}
+				if(preg_match('#phone_label_([0-9]*)#', $key, $matches)) $all_phones_labels[$matches[1]] = $value;
+			}
+		}
+
+		// Validation du formulaire
+		if($this->form_validation->run() AND empty($phone_errors) AND empty($mail_errors)){
+			$theater_name = $this->input->post('name');
+			$theater_city = $this->input->post('city');
+			$theater_address = $this->input->post('address');
+			$theater_postal_code = $this->input->post('postal_code');
+			$this->theatersManager->add_theater($theater_name, $theater_city, $theater_address, $theater_postal_code);
+		} else{
+			# On vérifie si il y a des erreurs :
+			$data['form']['all_mails'] = (empty($all_mails)) ? null : $all_mails;
+			$data['form']['all_mails_labels'] = (empty($all_mails_labels)) ? null : $all_mails_labels;
+			$data['form']['mail_errors'] = (empty($mail_errors)) ? null : $mail_errors;
+			$data['form']['mail_sucess'] = (empty($mails)) ? null : $mails;
+
+			$data['form']['all_phones'] = (empty($all_phones)) ? null : $all_phones;
+			$data['form']['all_phones_labels'] = (empty($all_phones_labels)) ? null : $all_phones_labels;
+			$data['form']['phone_errors'] = (empty($phone_errors)) ? null : $phone_errors;
+			$data['form']['phone_sucess'] = (empty($phone_numbers)) ? null : $phone_numbers;
+
+			$this->load->view('templates/header');
+			$this->load->view('dashboard/theaters/add', $data);
+			$this->load->view('templates/footer');
+		}
+	}
+}
+
+/* End of file theaters.php */
+/* Location: ./application/controllers/dashboard/theaters.php */
---
+++ application/models/comedians_model.php
@@ -0,0 +1,101 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Theaters_model extends CI_Model
+{
+    protected $table = 'theaters';
+    protected $labels_table = 'labels';
+    protected $phones_table = 'theaters_phones';
+    protected $mails_table = 'theaters_mails';
+ 
+    public function add_theater($name, $city, $address, $postal_code)
+    {
+        return $this->db->set('name',   $name)
+                    ->set('city',   $city)
+                ->set('address', $address)
+                ->set('postal_code', $postal_code)
+                ->set('created_at', time(), false)
+                ->set('updated_at', time(), false)
+                ->insert($this->table);
+    }
+     
+    public function edit_theater($id, $titre = null, $contenu = null)
+    {
+        //  Il n'y a rien à éditer
+        if($titre == null AND $contenu == null)
+        {
+            return false;
+        }
+         
+        //  Ces données seront échappées
+        if($titre != null)
+        {
+            $this->db->set('titre', $titre);
+        }
+        if($contenu != null)
+        {
+            $this->db->set('contenu', $contenu);
+        }
+         
+        return $this->db->set('date_modif', 'NOW()', false)
+                ->where('id', (int) $id)
+                ->update($this->table);
+    }
+     
+
+    public function delete_theater($id)
+    {
+        return $this->db->where('id', (int) $id)
+                ->delete($this->table);
+    }
+     
+
+    public function count($where = array())
+    {
+        return (int) $this->db->where($where)
+                      ->count_all_results($this->table);
+    }
+
+    public function list_theaters($nb = 10, $debut = 0)
+    {
+        return $this->db->select('*')
+                ->from($this->table)
+                ->limit($nb, $debut)
+                ->order_by('id', 'desc')
+                ->get()
+                ->result();
+    }
+
+    public function find_labels(){
+        return $this->db->select('*')->from($this->labels_table)->order_by('id', 'ASC')->get()->result();
+    }
+
+    public function add_phones_and_emails($contacts){
+        foreach ($contacts as $value) {
+            if(preg_match("#^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$#ix", $value)){
+                $this->add_mail($value['data']);
+            }
+            elseif(preg_match('#^(01|02|03|04|05|06|07|08|09)[0-9]{8}$#', $value)){
+                $this->add_phone($value['data']);
+            }
+        }
+    }
+
+    public function add_phone($phones){
+         return $this->db->set('name',   $name)
+                    ->set('city',   $city)
+                ->set('address', $address)
+                ->set('postal_code', $postal_code)
+                ->set('phone', $phone)
+                ->set('date_added', 'NOW()', false)
+                ->set('date_modified', 'NOW()', false)
+                ->insert($this->table);
+    }
+
+    public function add_mail($mails){
+
+    }
+}
+ 
+ 
+/* End of file theaters_model.php */
+/* Location: ./application/models/theaters_model.php */
